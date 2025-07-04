dd409b54254aba273a3ed10d7253c5110b911c98
Jaak Tamm <jaaktamm@live.com>
2025-07-04 19:40:27 +0300

    Python basic

---
+++ hello.py
@@ -0,0 +1,20 @@
+print("Hello, SpaceX Data Engineer!")
+
+# Integer
+launch_count = 10
+
+# Float
+success_rate = 0.95
+
+# String
+rocket_name = "Falcon 9"
+
+# Boolean
+is_active = True
+
+total_launches = launch_count + 5
+
+print(total_launches)
+
+# This is a comment
+
---
+++ variables.py
@@ -0,0 +1,12 @@
+# Number of launches
+launches = 9
+
+# Name of rocket
+rocket_name = "Lennuk"
+
+# Launch success rate
+success_rate = 0.85
+
+print(launches)
+print(rocket_name)
+print(success_rate)
