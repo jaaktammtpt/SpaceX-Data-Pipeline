06255e739e82e3817efe40a71e72d1665e6ae1b4
Jaak Tamm <jaaktamm@live.com>
2025-07-05 18:35:55 +0300

    Pandas intro

---
+++ data_manipulation.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+data = {
+    'rocket': [
+        'Falcon 1',
+        'Falcon 9',
+        'Falcon Heavy',
+    ],
+    'launches': [5, 100, 3],
+}
+
+df = pd.DataFrame(data)
+
+# print(df)
+
+#print(df['rocket'])
+
+# falcon9_df = df[df['rocket'] == 'Falcon 9']
+
+df['sucess_rate'] = [0.4, 0.98, 1.0]
+
+print(df)
